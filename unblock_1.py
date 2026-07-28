69205a5a79d2aa9057ecb0fea4a508935b2bddf9
Dakxh Dakxh <89646853+dakxh@users.noreply.github.com>
2026-07-28 13:50:30 +0530

    Change unblocked seats threshold from 9 to 7

--- unblock_1.py
+++ unblock_1.py
@@ -341,7 +341,7 @@ def main():
                 
                 if not is_first_run:
                     # Check if the unblocked seats meet the minimum threshold of 6
-                    if newly_unblocked_count >= 9:
+                    if newly_unblocked_count >= 7:
                         rows_str = ", ".join(sorted(unblocked_rows_list))
                         human_date = humanize_date(s_date)
                         
